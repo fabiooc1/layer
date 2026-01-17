9f5e50a3adc0b56374dac82d35fc5fe33c795a8e
fabiooc1 <fabiocaldas266@gmail.com>
2026-01-17 13:24:59 -0300

    app layout

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  variable: "--font-poppins",
+  weight: ["400", "500", "600", "700"],
+});
+
+export const metadata: Metadata = {
+  title: "Layer",
+  description: "Um e-commerce para a venda de roupas e acessórios online.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body className={`${poppins.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
