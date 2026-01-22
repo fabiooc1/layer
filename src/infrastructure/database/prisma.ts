9c629f931e426a9195fede64bd78bfb82a0a29bd
fabiooc1 <fabiocaldas266@gmail.com>
2026-01-21 22:22:32 -0300

    change prisma directory

--- src/infrastructure/database/prisma.ts
+++ src/infrastructure/database/prisma.ts
@@ -1,5 +1,5 @@
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../../../prisma/generated/client";
+import { PrismaClient } from "../../../generated/prisma/client";
 
 export const prisma = new PrismaClient({
   adapter: new PrismaPg({
