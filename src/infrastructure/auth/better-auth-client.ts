82e64d8a6461221bdd367ba629c0a3c0de42d2fc
fabiooc1 <fabiocaldas266@gmail.com>
2026-01-21 22:15:55 -0300

    better auth removed

--- src/infrastructure/auth/better-auth-client.ts
+++
@@ -1,6 +0,0 @@
-import { createAuthClient } from "better-auth/react";
-
-export const betterAuthClient = createAuthClient({
-  /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: process.env!.BASE_URL,
-});
