ac3a1c3840e3cb302f001aa5b98e3c50f1592448
fabiooc1 <fabiocaldas266@gmail.com>
2026-01-21 22:17:15 -0300

    showing header

--- src/app/(landing-page)/page.tsx
+++ src/app/(landing-page)/page.tsx
@@ -1,3 +1,10 @@
-export default function LandingPage() {
-  return <main></main>
+import { Header } from "@/components/header";
+import { getUserAction } from "../actions/get-user-action";
+
+export default async function LandingPage() {
+  return (
+    <div className="space-y-6">
+      <Header />
+    </div>
+  );
 }
